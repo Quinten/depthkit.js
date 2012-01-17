c77bcfadcbd1c9a3e5cc01c19bf13168e0596bc4
Quinten Clause <quintenclause@hotmail.com>
2012-01-17 22:27:30 +0100

    index

--- builds/depthkit.js
+++ builds/depthkit.js
@@ -902,13 +902,31 @@ DepthKit.mouse.attachTo = function (viewport) {
     mouse.event = e;
   };
   viewport.canvas.addEventListener('mousemove', viewport.mousemove, false);
+  viewport.touchmove = function (e) {
+    var x, y;
+    if (e.touches[0].pageX || e.touches[0].pageY) {
+      x = e.touches[0].pageX;
+      y = e.touches[0].pageY;
+    } else {
+      x = e.touches[0].clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+      y = e.touches[0].clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+    x -= viewport.canvas.offsetLeft;
+    y -= viewport.canvas.offsetTop;
+    mouse.x = x;
+    mouse.y = y;
+    mouse.event = e;
+  };
+  viewport.canvas.addEventListener('touchmove', viewport.touchmove, false);
   viewport.mouse = mouse;
 }
 
 DepthKit.mouse.detachFrom = function (viewport) {
   viewport.canvas.removeEventListener('mousemove', viewport.mousemove, false);
+  viewport.canvas.removeEventListener('touchmove', viewport.touchmove, false);
   delete viewport.mouse;
   delete viewport.mousemove;
+  delete viewport.touchmove;
 }
 
 DepthKit.Engine = function () {
